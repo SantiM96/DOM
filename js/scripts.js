817f94ad6c744af20a1bb1719ee4e5483bb7f39e
SantiM96 <smt093@gmail.com>
2020-07-28 20:43:30 -0300

    Practice with js (Add "Entrada 6")

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,40 @@
 (function(){
-  'use strict';
-  document.addEventListener('DOMContentLoaded', function(){
+    'use strict';
+    document.addEventListener('DOMContentLoaded', function(){
     
+        // Añadir "Entrada 6" al aside
+
+        var sidebar = document.querySelectorAll('aside ul');
+
+        var newEnter = document.createElement('a');
+        var newEnterInner = document.createTextNode('Entrada 6');
+        newEnter.setAttribute('href', '#');
+        newEnter.appendChild(newEnterInner);
+
+
+        var newEnterList = document.createElement('li');
+        newEnterList.appendChild(newEnter);
     
+
+        sidebar[0].appendChild(newEnterList);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       
       
     
     
-  });
+    });
   
 })();
